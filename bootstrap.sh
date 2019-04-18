6f17a43a8580b8ff38ad99ca66cde7004f1787c4
jp <juaumlol@gmail.com>
2019-04-18 00:36:00 -0300

    instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -63,3 +63,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 source .osx
+
+
+# :PlugInstall
+# :PluginInstall
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.py --all
